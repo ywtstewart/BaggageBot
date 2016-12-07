47f4a40647604ae30c20409e155cd2296bc07912
Quinnard Stewart <quinnard.stewart@gmail.com>
2016-12-07 19:21:55 +0100

    Page
    page up & running

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
+//ReactJS to send question and responses directly to the conversation container
+//var react = require('react');
+//var reactDOM = require('react-dom');
+
 var apiai = require('apiai');
  
 var app = apiai('d6e491b6f1754e9085b1704b0357fc06');
  
+//for random session id
+var session = Math.random()*100000000000; 
+//var question = document.getElementById("input").value; 
+var question = '';
+var defaultQuestion = 'lost baggage';
+var botResponse; 
 
-var request = app.textRequest('lost baggage', {
-    sessionId: '12345'
+if (question) {
+     sendRequest(question); 
+}else {
+sendRequest(defaultQuestion); }
+
+var request; 
+
+function sendRequest (question) {
+	console.log('request sent');
+
+	request = app.textRequest(question, { 
+	sessionId: session
 });
- 
+
 request.on('response', function(response) {
-    console.log(response);
+	//Responded message from bot is within speech 
+	botresponse = response.result.fulfillment.speech;
+	
+	console.log(botresponse);
+	//setResponse(botResponse, document.getElementById('conv_list')); 
+	
 });
  
 request.on('error', function(error) {
@@ -16,3 +41,11 @@ request.on('error', function(error) {
 });
  
 request.end();
+
+/*function setResponse(message, container){
+	return ( '<li>'+ message + '</li>'); 
+ 
+}*/
+
+}
+
